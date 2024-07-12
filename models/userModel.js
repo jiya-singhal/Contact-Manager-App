c4995de7e6491f24caa48be28dd969360ceae059
Jiya Singhal <jiyasinghal95@gmail.com>
2024-07-13 04:52:04 +0530

    Updated user model and routes with error handling

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please add the user name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add the user email address"],
+        unique: [true, "Email address already taken"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add the user password"]
+    }
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require("express");
-
+const errorHandler = require("./middleware/errorHandler");
+const connectdb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
-
-const app = express()
+connectdb();
+const app = express();
 
 const port = process.env.PORT || 5000;
 
+// Middleware to parse JSON
+app.use(express.json());
 
+// Routes
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+
+// Error handling middleware
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-
 });
