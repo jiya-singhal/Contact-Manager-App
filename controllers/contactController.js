101d10729e4b0ea3860471815781bebb957e72f4
Jiya Singhal <jiyasinghal95@gmail.com>
2024-07-19 02:53:38 +0530

    Add/update contact functionality, fix issues with delete operation

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,19 +3,19 @@ const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 const getContacts = asyncHandler(async (req, res) => {
-    const contacts = await Contact.find();
+    const contacts = await Contact.find({ user_id: req.user.id });
     res.status(200).json(contacts);
 });
 
 //@desc Create new contact
 //@route POST /api/contacts
-//@access public
+//@access private
 const createContact = asyncHandler(async (req, res) => {
     console.log("The request body is", req.body);
-    const {name,email,phone} = req.body;
-    if(!name || !email || !phone) {
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
         res.status(400);
         throw new Error("All fields are mandatory");
     }
@@ -23,52 +23,65 @@ const createContact = asyncHandler(async (req, res) => {
         name,
         email,
         phone,
-    })
+        user_id: req.user.id,
+    });
     res.status(201).json(contact);
 });
 
 //@desc Get contact
 //@route GET /api/contacts/:id
-//@access public
+//@access private
 const getContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
-    if(!contact) {
+    if (!contact) {
         res.status(404);
         throw new Error("Contact not found");
+    }
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User not authorized to view this contact");
     }
     res.status(200).json(contact);
 });
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
-//@access public
+//@access private
 const updateContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
-    if(!contact) {
+    if (!contact) {
         res.status(404);
         throw new Error("Contact not found");
+    }
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User not authorized to update this contact");
     }
 
     const updatedContact = await Contact.findByIdAndUpdate(
         req.params.id,
         req.body,
-        {new: true}
+        { new: true }
     );
     res.status(200).json(updatedContact);
 });
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
-//@access public
+//@access private
 const deleteContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
-    if(!contact) {
+    if (!contact) {
         res.status(404);
         throw new Error("Contact not found");
     }
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User not authorized to delete this contact");
+    }
 
-    await Contact.remove();
-    res.status(200).json(contact);
+    await Contact.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Contact deleted successfully" });
 });
 
 module.exports = { getContacts, createContact, getContact, updateContact, deleteContact };
